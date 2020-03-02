4bfe86cae0fd41102e0fe90fc7f936651a6bfaf4
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-03-02 18:11:36 -0400

    ajustes

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,7 @@ Route::get('/', function () {
 /* Route::get('/login', function () {
     return view('auth.login');
 })->name('login'); */
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
